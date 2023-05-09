54df428d0c7328fb9b59cec10f560eb8fd4e3686
IS <1588186+issarapong@users.noreply.github.com>
2023-05-09 17:07:04 +0700

    Finish:Request by id http server

--- lab07/index.js
+++ lab07/index.js
@@ -2,11 +2,33 @@ const http = require('http')
 
 const server =http.createServer()
 
+
+const todo =[
+    
+        {id:1, title : 'xxx'},
+        {id:2, title : 'xxssx'},
+        {id:3, title : 'xssxx'},    
+]
+
+
 server.listen(8000, ()=> console.log('server on 8000'))
 
 server.on('request', (req,res)=>  {
     console.log('Method :', req.method)
-    console.log('path :'), req.url
+    console.log('path : ',req.url)
+
+     if(req.url.startsWith('/todo')) {
+        let param =req.url.split('/').pop()
+        let id = +param
+        let result = todo.find(el => el.id === id)
+        res.statusCode = 200
+        res.setHeader('content-type', 'application/json')
+        res.write(JSON.stringify(result))
+        return res.end()
+     }
+
+
+
     res.statusCode =200
     //res.setHeader('content-type', 'application/json')
     res.setHeader('content-type', 'text/plain')
