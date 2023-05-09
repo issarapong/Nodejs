fd05bda4cfb9d19d02fce1a8be95d0951d1f0046
IS <1588186+issarapong@users.noreply.github.com>
2023-05-09 11:49:13 +0700

    ดัก error แบบ asynce

--- lab04/index.js
+++ lab04/index.js
@@ -4,23 +4,23 @@ const fsa = require ('fs/promises')
 
 
 
-console.log("-------------Sync-----------------")
-//console.time('readSynce') //เริ่มต้นจับเวลา
+// console.log("-------------Sync-----------------")
+// //console.time('readSynce') //เริ่มต้นจับเวลา
 
-try{  // ใช้ try catch  เพื่อ ดักจับ error แล้วให้ process อื่นทำงานต่อ /file01.tx
-//let data1 =fs.readFileSync('./file01.txt', 'base64')
-let data1 =fs.readFileSync('./file01.tx', 'utf8')
-console.log(data1)
-}catch(err){
-    console.log(err.message)
-}
-let data2 =fs.readFileSync('./file02.txt', 'utf8')
-console.log(data2)
+// try{  // ใช้ try catch  เพื่อ ดักจับ error แล้วให้ process อื่นทำงานต่อ /file01.tx
+// //let data1 =fs.readFileSync('./file01.txt', 'base64')
+// let data1 =fs.readFileSync('./file01.tx', 'utf8')
+// console.log(data1)
+// }catch(err){
+//     console.log(err.message)
+// }
+// let data2 =fs.readFileSync('./file02.txt', 'utf8')
+// console.log(data2)
 
-let data3 =fs.readFileSync('./file03.txt', 'utf8')
-console.log(data3)
+// let data3 =fs.readFileSync('./file03.txt', 'utf8')
+// console.log(data3)
 
-//console.timeEnd('readSynce') // 
+// //console.timeEnd('readSynce') // 
 
 
 
@@ -32,7 +32,16 @@ console.log(data3)
 //------------Asyncrous callback--------
 // console.log("--------------Async callback-----------------")
 // console.time('readCB')
-// fs.readFile('./file01.txt', 'utf8', (err, data) => console.log(data))
+// fs.readFile('./filae01.txt', 'utf8', (err, data) =>  {  //ดักจับ errorอีกแบบ
+//     if(err) {
+//         console.log(err.message)
+//         return
+//     }
+//     console.log(data)
+    
+// })
+
+
 // fs.readFile('./file02.txt', 'utf8', (err, data) => console.log(data))
 // fs.readFile('./file03.txt', 'utf8', (err, data) => console.log(data))
 // console.timeEnd('readCB')
