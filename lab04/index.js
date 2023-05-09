3b933c9a9ad388b1ffa559aaf2de5a3fe82e29e8
IS <1588186+issarapong@users.noreply.github.com>
2023-05-09 11:42:29 +0700

    try catch ดักจับ error

--- lab04/index.js
+++ lab04/index.js
@@ -7,10 +7,13 @@ const fsa = require ('fs/promises')
 console.log("-------------Sync-----------------")
 //console.time('readSynce') //เริ่มต้นจับเวลา
 
+try{  // ใช้ try catch  เพื่อ ดักจับ error แล้วให้ process อื่นทำงานต่อ /file01.tx
 //let data1 =fs.readFileSync('./file01.txt', 'base64')
 let data1 =fs.readFileSync('./file01.tx', 'utf8')
 console.log(data1)
-
+}catch(err){
+    console.log(err.message)
+}
 let data2 =fs.readFileSync('./file02.txt', 'utf8')
 console.log(data2)
 
@@ -27,19 +30,19 @@ console.log(data3)
 
 
 //------------Asyncrous callback--------
-console.log("--------------Async callback-----------------")
-console.time('readCB')
-fs.readFile('./file01.txt', 'utf8', (err, data) => console.log(data))
-fs.readFile('./file02.txt', 'utf8', (err, data) => console.log(data))
-fs.readFile('./file03.txt', 'utf8', (err, data) => console.log(data))
-console.timeEnd('readCB')
+// console.log("--------------Async callback-----------------")
+// console.time('readCB')
+// fs.readFile('./file01.txt', 'utf8', (err, data) => console.log(data))
+// fs.readFile('./file02.txt', 'utf8', (err, data) => console.log(data))
+// fs.readFile('./file03.txt', 'utf8', (err, data) => console.log(data))
+// console.timeEnd('readCB')
 
-//------------Asyncrous promise--------
-console.log("--------------Async promise-----------------")
+// //------------Asyncrous promise--------
+// console.log("--------------Async promise-----------------")
 
-console.time('readPM')
-fsa.readFile('./file01.txt','utf8').then( data => console.log(data))
-fsa.readFile('./file02.txt','utf8').then( data => console.log(data))
-fsa.readFile('./file03.txt','utf8').then( data => console.log(data))
-console.timeEnd('readPM')
+// console.time('readPM')
+// fsa.readFile('./file01.txt','utf8').then( data => console.log(data))
+// fsa.readFile('./file02.txt','utf8').then( data => console.log(data))
+// fsa.readFile('./file03.txt','utf8').then( data => console.log(data))
+// console.timeEnd('readPM')
 
